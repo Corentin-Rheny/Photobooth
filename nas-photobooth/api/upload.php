68864f59ffe0850c4141f49342df623fdb68c781
Corentin <73130881+Corentin-Rheny@users.noreply.github.com>
2026-06-30 09:57:21 +0200

    Add NAS upload endpoint

---
+++ nas-photobooth/api/upload.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . '/../config.php';
+header('Content-Type: application/json; charset=utf-8');
+
+$photoDir = __DIR__ . '/../photos';
+if (!is_dir($photoDir)) {
+    mkdir($photoDir, 0755, true);
+}
+
+if (!isset($_FILES['photo']) || $_FILES['photo']['error'] !== UPLOAD_ERR_OK) {
+    http_response_code(400);
+    echo json_encode(['ok' => false, 'error' => 'Aucune photo reçue']);
+    exit;
+}
+
+$stamp = date('Ymd-His');
+$rand = substr(bin2hex(random_bytes(4)), 0, 6);
+$filename = 'GC-' . $stamp . '-' . $rand . '.png';
+$target = $photoDir . '/' . $filename;
+
+if (!move_uploaded_file($_FILES['photo']['tmp_name'], $target)) {
+    http_response_code(500);
+    echo json_encode(['ok' => false, 'error' => 'Impossible d’enregistrer la photo']);
+    exit;
+}
+
+@chmod($target, 0644);
+$url = PUBLIC_BASE_URL . '/photo.php?f=' . rawurlencode($filename);
+echo json_encode(['ok' => true, 'filename' => $filename, 'url' => $url]);
